efc04831ff75d8d6624704d93cc06253f20f25b0
Ашихмин Артем Валерьевич <a.ashikhmin@mkskom.ru>
2021-04-23 23:13:58 +0300

    Changes by v.klimentovskiy

---
+++ smevil-client/src/main/java/ru/otvertka352/smevilclient/api/dto/ConditionsDTO.java
@@ -0,0 +1,13 @@
+package ru.otvertka352.smevilclient.api.dto;
+
+import lombok.Data;
+
+@Data
+public class ConditionsDTO {
+    private String condition1;
+    private String condition2;
+    private String condition3;
+    private String condition4;
+    private String condition5;
+    private String condition6;
+}
