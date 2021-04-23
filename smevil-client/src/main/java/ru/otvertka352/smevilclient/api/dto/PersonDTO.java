ba9e7c2c62cc9b4e7d435459ce7db71a2a08a71e
Ашихмин Артем Валерьевич <a.ashikhmin@mkskom.ru>
2021-04-23 23:13:58 +0300

    Changes by v.klimentovskiy

---
+++ smevil-client/src/main/java/ru/otvertka352/smevilclient/api/dto/PersonDTO.java
@@ -0,0 +1,10 @@
+package ru.otvertka352.smevilclient.api.dto;
+
+import lombok.Data;
+
+@Data
+public class PersonDTO {
+    private String role;
+    private String famio;
+    private String contactInfo;
+}
--- smevil-client/src/main/java/ru/otvertka352/smevilclient/impl/handler/impl/XLSXReaderRPZ.java
+++ smevil-client/src/main/java/ru/otvertka352/smevilclient/impl/handler/impl/XLSXReaderRPZ.java
@@ -1,9 +1,11 @@
 package ru.otvertka352.smevilclient.impl.handler.impl;
 
-import org.apache.poi.xssf.usermodel.XSSFName;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFTable;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.ss.SpreadsheetVersion;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.util.AreaReference;
+import org.apache.poi.ss.util.CellReference;
+import org.apache.poi.xssf.usermodel.*;
 import ru.otvertka352.smevilclient.impl.handler.XLSXReader;
 
 import java.io.ByteArrayInputStream;
@@ -19,7 +21,45 @@ public class XLSXReaderRPZ implements XLSXReader {
             XSSFSheet sheet = workbook.getSheetAt(0);
             final List<XSSFName> allNames = workbook.getAllNames();
             final List<XSSFTable> sheetTables = sheet.getTables();
-            System.out.println("sdfsdf");
+
+            for (XSSFName field: allNames
+                 ) {
+                System.out.println(field.getRefersToFormula());
+                AreaReference areaRef;
+                try {
+                    areaRef = new AreaReference(field.getRefersToFormula(), SpreadsheetVersion.EXCEL2007);
+                }catch (IllegalArgumentException e){
+                    //TODO: Add log record
+                    continue;
+                }
+
+                if (areaRef.isSingleCell()){
+                    CellReference cellRef = areaRef.getFirstCell();
+                    XSSFRow row = sheet.getRow(cellRef.getRow());
+                    XSSFCell cell = row.getCell(cellRef.getCol());
+                    CellType cellType = cell.getCellType();
+                    if (cellType == CellType.STRING){
+                        System.out.println(cell.getStringCellValue());
+                    }
+                }
+
+
+            }
+
+            String testRefFormula = allNames.get(0).getRefersToFormula();
+            AreaReference areaRef = new AreaReference(testRefFormula, SpreadsheetVersion.EXCEL2007);
+
+            if (areaRef.isSingleCell()){
+                CellReference cellRef = areaRef.getFirstCell();
+                XSSFRow row = sheet.getRow(cellRef.getRow());
+                XSSFCell cell = row.getCell(cellRef.getCol());
+                System.out.println('t');
+            }
+
+
+            System.out.println(testRefFormula);
+
+
 
         } catch (IOException e) {
             e.printStackTrace();
