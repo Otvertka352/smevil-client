aa61e665c4275c4dcd3c42ec73c96acfb4a4c1be
Otvertka352 <otvertka352@gmail.com>
2021-04-26 23:13:33 +0300

    with err

--- smevil-client/src/main/java/ru/otvertka352/smevilclient/impl/util/ExcelUtil.java
+++ smevil-client/src/main/java/ru/otvertka352/smevilclient/impl/util/ExcelUtil.java
@@ -21,22 +21,37 @@ public class ExcelUtil {
     public AreaReference getAreaReference(XSSFName field) {
         try {
             return new AreaReference(field.getRefersToFormula(), SpreadsheetVersion.EXCEL2007);
-        }catch (IllegalArgumentException e){
-            return null;}
+        } catch (IllegalArgumentException e) {
+            return null;
+        }
 
     }
 
     public static FieldValue getFieldValue(XSSFName field, XSSFSheet sheet) {
-        final AreaReference areaReference = getAreaReference(field);
-        if (areaReference.isSingleCell()){
-            CellReference cellRef = areaReference.getFirstCell();
-            XSSFRow row = sheet.getRow(cellRef.getRow());
-            XSSFCell cell = row.getCell(cellRef.getCol());
-            CellType cellType = cell.getCellType();
-            if (cellType == CellType.STRING){
-                System.out.println(cell.getStringCellValue());
-            }
-        }
+//        final AreaReference areaReference = getAreaReference(field);
+//        FieldValue value = null;
+//        if (areaReference.isSingleCell()) {
+//            XSSFCell cell = getXssfCell(sheet, areaReference);
+//            switch (cell.getCellType()) {
+//                case STRING:
+//                    value = new FieldValue<String>("dfdf");
+//                    break;
+//                case NUMERIC:
+//                    value = new FieldValue<Integer>(1);
+//                    break;
+//                default:
+//                    throw new IllegalStateException("Unexpected value: " + cell.getCellType());
+//            }
+//        }
         return null;
     }
+
+    private static XSSFCell getXssfCell(XSSFSheet sheet, AreaReference areaReference) {
+        CellReference cellRef = areaReference.getFirstCell();
+        XSSFRow row = sheet.getRow(cellRef.getRow());
+        XSSFCell cell = row.getCell(cellRef.getCol());
+        return cell;
+    }
 }
+
+
